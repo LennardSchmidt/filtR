3f3f90b70596fdcfbed59c2fd9d6f4c7a877e92b
LennardSchmidt <lennardschmidt@outlook.com>
2019-11-06 00:59:54 +0000

    Changed Namespace

--- R/hello.R
+++ R/hello.R
@@ -10,46 +10,50 @@
 #' @param x input from wrapper
 #' @inheritParams valid
 #'
+#' @import rlang
+#' @import dplyr
+#' @import pwr
+#' @import tibble
 #' @importFrom magrittr %>%
-#' @importFrom rlang .data
+#' @importFrom effsize cohen.d
 #'
 #' @return a tibble with validity criteria
 
 subset_fun <- function(x) {
 
     names <- names(x)
-    data <- tibble::as_tibble(.data$df)
+    data <- as_tibble(.data$df)
 
     # Create subset
     for (name in names) {
         val <- x[[name]]
 
         if (is.factor(val)) {
-            data <- data %>% dplyr::filter(!!rlang::sym(name) == val)
+            data <- data %>% filter(!!sym(name) == val)
 
         } else if (is.numeric(val)) {
-            data <- data %>% dplyr::filter(!!rlang::sym(name) <= val)
+            data <- data %>% filter(!!sym(name) <= val)
 
         }
     }
 
     # Select variables
-    data <- data %>% dplyr::select(.data$target1, .data$target2) %>% tibble::as_tibble()
+    data <- data %>% select(.data$target1, .data$target2) %>% as_tibble()
 
     # Calculate metrics
     if (!is.null(.data$target2)) {
         n <- nrow(data)
-        esize <- effsize::cohen.d(data[[.data$target1]], data[[.data$target2]], na.rm = T)$estimate
-        pwr <- pwr::pwr.t.test(n = n, d = esize, type = "paired")$power
+        esize <- cohen.d(data[[.data$target1]], data[[.data$target2]], na.rm = T)$estimate
+        pwr <- pwr.t.test(n = n, d = esize, type = "paired")$power
     } else {
         n1 <- table(data[[.data$groupvar]])[1]
         n2 <- table(data[[.data$groupvar]])[2]
-        esize <- effsize::cohen.d(data[[.data$target1]], data[[.data$groupvar]])
-        pwr <- pwr::pwr.t2n.test(n = n1, n2 = n2, d = esize)$power
+        esize <- cohen.d(data[[.data$target1]], data[[.data$groupvar]])
+        pwr <- pwr.t2n.test(n = n1, n2 = n2, d = esize)$power
     }
 
     # Store metrics
-    results <- tibble::tibble(`Sample Size` = n, Power = pwr, `Effect Size` = esize)
+    results <- tibble(`Sample Size` = n, Power = pwr, `Effect Size` = esize)
 
     return(results)
 }
@@ -63,8 +67,12 @@ subset_fun <- function(x) {
 #'
 #' @retun a tibble including
 #'
+#' @import dplyr
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
+#' @importFrom tibble as_tibble
+#' @importFrom purrr map
+#' @importFrom tidyr unnest
 #'
 #' @examples
 #' library(filtR)
@@ -76,15 +84,15 @@ subset_fun <- function(x) {
 valid <- function(target1 = target1, target2 = NULL, groupvar = NULL, df = df) {
 
     # Split predictor variables
-    predictors <- df %>% dplyr::select(-.data$target1, -.data$target2)
+    predictors <- df %>% select(-.data$target1, -.data$target2)
 
     # Generate all possible predictor-value combinations
     comb <- predictors %>% expand.grid() %>% unique()
 
     # For every predictor-value combination subset dataframe and append as list
-    output <- comb %>% dplyr::rowwise() %>% dplyr::do(row = tibble::as_tibble(.data$.)) %>% tibble::as_tibble %>% dplyr::mutate(subset = purrr::map(row, subset_fun))
+    output <- comb %>% rowwise() %>% do(row = as_tibble(.data$.)) %>% as_tibble %>% mutate(subset = map(row, subset_fun))
 
     # #Map over list and generate validity criteria
-    results <- output %>% tidyr::unnest(subset)
+    results <- output %>% unnest(subset)
 
 }
